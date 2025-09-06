1e308197d279c9183ab2b9568ebac5308e77b627
Lari Nurkka <lari.nurkka@gmail.com>
2025-09-06 20:12:07 +0000

    fetch and use all missing dependencies

--- link-node-dependencies.sh
+++ link-node-dependencies.sh
@@ -2,5 +2,7 @@
 
 rm -Rf assets/node && mkdir -p assets/node
 link node_modules/bootstrap/dist/js/bootstrap.bundle.min.js assets/node/bootstrap.bundle.min.js
-link node_modules/jquery/dist/jquery.min.js assets/node/query.min.js
+link node_modules/bootstrap/dist/css/bootstrap.min.css assets/node/bootstrap.min.css
+link node_modules/jquery/dist/jquery.min.js assets/node/jquery.min.js
+link node_modules/popper.js/dist/popper.min.js assets/node/popper.min.js
 link node_modules/jquery.easing/jquery.easing.min.js assets/node/jquery.easing.min.js
